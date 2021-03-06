128775514b1a58690e0d8a9f9bc052520a157ac4
Arnab Dutta <arnab.priyam@gmail.com>
2021-03-06 21:52:52 +0530

    Section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,9 @@ export class AccountService {
       map((response: User) =>{
         const user = response;
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          // localStorage.setItem('user',JSON.stringify(user));
+          // this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );  
@@ -31,8 +32,10 @@ export class AccountService {
     return this.http.post(this.baseUrl+ 'account/register', model).pipe(
       map((user : User) => {
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          // localStorage.setItem('user',JSON.stringify(user));
+          // this.currentUserSource.next(user);
+
+          this.setCurrentUser(user);
         }
         //return user;
       })
@@ -40,6 +43,7 @@ export class AccountService {
   }
 
   setCurrentUser(user: User){
+    localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
